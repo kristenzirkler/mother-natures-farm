94834b89fbc297fca3cb1813cfa4d627b93e9f4b
Kristen Zirkler <bazirkler@gmail.com>
2017-09-02 12:57:08 -0700

    fix key

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ $(document).foundation()
 
 $(document).ready(function() {
     $('#calendar').fullCalendar({
-        googleCalendarApiKey: '733530201410-0ef7ks1jnad8jl2cnp3qnegcr2ilbq42.apps.googleusercontent.com',
+        googleCalendarApiKey: 'AIzaSyBTWyqDSnCYLq13LDKyk0D7zCLlYEJw-vA',
         events: {
             googleCalendarId: '1rbbvflkj20qog42qoh6re92a8@group.calendar.google.com',
             className: 'gcal-event' // an option!
