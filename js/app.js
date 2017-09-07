e57f8bfe361792506b120d2ea210d5458ef6d53a
Kristen Zirkler <bazirkler@gmail.com>
2017-09-07 14:15:45 -0700

    header footer mobile

--- js/app.js
+++ js/app.js
@@ -1,11 +1,20 @@
 $(document).foundation()
 
 $(document).ready(function() {
-    $('#calendar').fullCalendar({
-        googleCalendarApiKey: 'AIzaSyBTWyqDSnCYLq13LDKyk0D7zCLlYEJw-vA',
-        events: {
-            googleCalendarId: '1rbbvflkj20qog42qoh6re92a8@group.calendar.google.com'
-        }
-        
-    });
+	if ($('body').hasClass('calendar')) {
+	    $('#calendar').fullCalendar({
+	        googleCalendarApiKey: 'AIzaSyBTWyqDSnCYLq13LDKyk0D7zCLlYEJw-vA',
+	        events: {
+	            googleCalendarId: '1rbbvflkj20qog42qoh6re92a8@group.calendar.google.com'
+	        }
+	        
+	    });
+	}
+	// Mobile Nav
+	$('#menuToggle').click(function(event) {
+	    event.preventDefault();
+		$('body').toggleClass('main-nav-open');
+		$(this).toggleClass('open');
+	});
+
 });
